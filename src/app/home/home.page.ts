e883eed2a10368c06fc6e21ae5d3d7db992bdbab
CodeWithAaditya <aaditya.sah365@gmail.com>
2021-08-04 21:37:41 -0400

    changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import * as bluetoothSerial from 'cordova-plugin-bluetooth-serial'
 import { PopoverController } from '@ionic/angular';
 import { BlepopoverComponent } from '../blepopover/blepopover.component';
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
 
 @Component({
   selector: 'app-home',
@@ -13,13 +14,21 @@ export class HomePage {
   letStates = [];
   constructor(public popoverController: PopoverController) { }
   ngOnInit() {
-    setInterval(() => {
-      this.readData();
-    }, 5000);
+    this.connect();
+    SpeechRecognition.requestPermission()
+      .then(
+        () => console.log('Granted'),
+        () => console.log('Denied')
+      )
 
-    setInterval(() => {
-      this.sendData("0,2,4,6,8");
-    }, 1000);
+  }
+
+  startListening() {
+    SpeechRecognition.startListening()
+      .subscribe(
+        (matches: Array<string>) => this.interpretCommand(matches[0]),
+        (onerror) => console.log('error:', onerror)
+      )
   }
   async presentPopover(ev: any) {
     const popover = await this.popoverController.create({
@@ -39,6 +48,29 @@ export class HomePage {
     return await popover.present();
   }
 
+  interpretCommand(command: string) {
+    if (command.includes('blue') && command.includes('off')) {
+      this.sendData('0');
+    } else if (command.includes('blue') && command.includes('on')) {
+      this.sendData('1');
+    } else if (command.includes('yellow') && command.includes('off')) {
+      this.sendData('2');
+    } else if (command.includes('yellow') && command.includes('on')){
+      this.sendData('3');
+    } else if (command.includes('red') && command.includes('off')){
+      this.sendData('4');
+    } else if (command.includes('red') && command.includes('on')){
+      this.sendData('5');
+    } else if (command.includes('green') && command.includes('off')){
+      this.sendData('6');
+    } else if (command.includes('green') && command.includes('on')){
+      this.sendData('7');
+    } else if (command.includes('white') && command.includes('off')){
+      this.sendData('8');
+    } else if (command.includes('white') && command.includes('on')){
+      this.sendData('9');
+    }
+  }
 
   connect() {
     bluetoothSerial.connect('00:21:13:00:A3:32');
